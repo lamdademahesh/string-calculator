9d5fb64804acf8659c5b8b816a13425cd832fccc
Mahesh Lamdade <mahesh.12lamdade@gmail.com>
2024-07-14 18:07:50 +0530

    test case(s) added : input string(s) comma separated & with nextline character

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -13,5 +13,17 @@ RSpec.describe StringCalculator do
         expect(StringCalculator.add("1")).to eq(1)
       end
     end
+
+    context 'when given multiple comma-separated numbers' do
+      it 'returns the sum of the numbers' do
+        expect(StringCalculator.add("1,2,3")).to eq(6)
+      end
+    end
+
+    context 'when numbers are separated by new lines' do
+      it 'allows new lines between numbers' do
+        expect(StringCalculator.add("1\n2,3")).to eq(6)
+      end
+    end
   end
 end
