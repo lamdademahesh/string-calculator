c3408126f4417d80811c6ca0ebc55b3810bb7c2c
Mahesh Lamdade <mahesh.12lamdade@gmail.com>
2024-07-14 18:09:33 +0530

    method updated to parse string that has nextline character(\n)

--- string_calculator.rb
+++ string_calculator.rb
@@ -3,6 +3,7 @@ class StringCalculator
     # Setting default delimiter
     delimiter = ','
 
+    input.gsub!("\n", delimiter)
     numbers = input.split(delimiter).map(&:to_i)
     numbers.sum
   end
