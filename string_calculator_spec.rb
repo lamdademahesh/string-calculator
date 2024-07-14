9cb4b2d3d3180ad1201f93c3bb06247957aafced
Mahesh Lamdade <mahesh.12lamdade@gmail.com>
2024-07-14 18:18:57 +0530

    test case : custom delimiter passed to add method

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -25,5 +25,11 @@ RSpec.describe StringCalculator do
         expect(StringCalculator.add("1\n2,3")).to eq(6)
       end
     end
+
+    context 'when numbers are separated by a custom delimiter' do
+      it 'supports custom delimiter' do
+        expect(StringCalculator.add("//;\n1;2")).to eq(3)
+      end
+    end
   end
 end
