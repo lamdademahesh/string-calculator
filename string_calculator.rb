edc859eb0874772b12c5e719ebff3c72976bf83f
Mahesh Lamdade <mahesh.12lamdade@gmail.com>
2024-07-14 17:57:34 +0530

    test case 1 passed for add method

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,2 +1,9 @@
-class StringCalculator  
+class StringCalculator
+  def self.add(input)
+    # Setting default delimiter
+    delimiter = ','
+
+    numbers = input.split(delimiter).map(&:to_i)
+    numbers.sum
+  end
 end
