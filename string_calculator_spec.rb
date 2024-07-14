988c6ae26000220dbd1734a8f241c1af20d56811
Mahesh Lamdade <mahesh.12lamdade@gmail.com>
2024-07-14 17:54:07 +0530

    test case added : empty string passed, expect result 0

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -1,4 +1,11 @@
 require_relative 'string_calculator'
 
 RSpec.describe StringCalculator do 
+  describe '.add' do 
+    context 'when given a empty string' do 
+      it 'returns 0' do 
+        expect(StringCalculator.add("")).to eq(0)
+      end
+    end
+  end
 end
