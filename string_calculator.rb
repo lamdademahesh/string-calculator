c9e3e946cccf113147e403081b07f47a20e30bf3
Mahesh Lamdade <mahesh.12lamdade@gmail.com>
2024-07-14 18:25:22 +0530

    custom delimiter support added to method

--- string_calculator.rb
+++ string_calculator.rb
@@ -3,6 +3,11 @@ class StringCalculator
     # Setting default delimiter
     delimiter = ','
 
+    if input.start_with?("//")
+      delimiter = input[2]
+      input = input[(input.index("\n") + 1)..-1]
+    end
+
     input.gsub!("\n", delimiter)
     numbers = input.split(delimiter).map(&:to_i)
     numbers.sum
